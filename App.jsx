87b0037aeb7a23a1f62c86948621ac11bae51040
Divyansh Kumar <shrivastav0885@gmail.com>
2025-06-09 13:32:04 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -39,37 +39,35 @@ function App() {
           </div>
           
           <ul>
-  {
-    todos.map((todo) => (
-      <li key={todo.id} 
-          className='flex items-center p-3 rounded-lg bg-slate-100 border-gray-200'>
-
-        <input
-          type="checkbox"
-          checked={todo.completed}
-          onChange={() => setTodos(
-            todos.map((t) =>
-              t.id === todo.id ? { ...t, completed: !t.completed } : t
-            )
-          )}
-          className='mr-2 h-5 w-5 text-blue-600 cursor-pointer'
-        />
-
-        <span className={`flex-grow ${todo.completed ? "line-through text-gray-500" : "text-gray-800"}`}>
-          {todo.text}
-        </span>
-
-        <button
-          onClick={() => setTodos(todos.filter((t) => t.id !== todo.id))}
-          className="ml-2 border-none p-2 rounded-lg bg-red-500 text-white hover:bg-red-600"
-        >
-          Delete
-        </button>
-      </li>
-    ))
-  }
-</ul>
-
+            {
+              todos.map((todo) => (
+                <li key={todo.id} 
+                    className='flex items-center p-3 rounded-lg bg-slate-100 border-gray-200'>
+          
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={() => setTodos(
+                      todos.map((t) =>
+                        t.id === todo.id ? { ...t, completed: !t.completed } : t
+                      )
+                    )}
+                    className='mr-2 h-5 w-5 text-blue-600 cursor-pointer'
+                  />
+          
+                  <span className={`flex-grow ${todo.completed ? "line-through text-gray-500" : "text-gray-800"}`}>
+                    {todo.text}
+                  </span>
+          
+                  <button
+                    onClick={() => setTodos(todos.filter((t) => t.id !== todo.id))}
+                    className="ml-2 border-none p-2 rounded-lg bg-red-500 text-white hover:bg-red-600">
+                    Delete
+                  </button>
+                </li>
+              ))
+            }
+          </ul>
 
         </div>
       </div>
